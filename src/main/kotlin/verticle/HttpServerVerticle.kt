14b67ee50b7657324d13fea6a434853ca722dd60
merizrizal <meriz.rizal@gmail.com>
2022-02-21 12:10:54 +0700

    Added another sites to be scraped

--- src/main/kotlin/verticle/HttpServerVerticle.kt
+++ src/main/kotlin/verticle/HttpServerVerticle.kt
@@ -1,19 +1,21 @@
 package verticle
 
 import io.vertx.core.Promise
+import io.vertx.core.impl.logging.LoggerFactory
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import io.vertx.rxjava3.core.AbstractVerticle
 import io.vertx.rxjava3.ext.web.Router
 import io.vertx.rxjava3.ext.web.RoutingContext
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 
 class HttpServerVerticle : AbstractVerticle() {
+    private val logger = LoggerFactory.getLogger(HttpServerVerticle::class.java)
+
     override fun start(promise: Promise<Void>) {
         val router = Router.router(vertx).apply {
-            get("/web-scraper/crawl").handler(this@HttpServerVerticle::crawl)
+            get("/web-scraper/scrape").handler(this@HttpServerVerticle::scrape)
         }
 
         vertx
@@ -25,41 +27,108 @@ class HttpServerVerticle : AbstractVerticle() {
                 { failure -> promise.fail(failure.cause) })
     }
 
-    private fun crawl(context: RoutingContext) {
+    private fun scrape(context: RoutingContext) {
         val responses = JsonArray()
-        // https://www.geekwire.com/startups/
-        // https://techstartups.com/
 
-
-        val document = Jsoup.connect("https://www.cnbc.com/startups/").get()
-        val pageRowElements = document.select("div.PageBuilder-pageRow")
-
-        for (pageRowElement in pageRowElements) {
-            val moduleHeaderTitle = pageRowElement.selectFirst("h2.ModuleHeader-title")
-
-            if (moduleHeaderTitle?.text().equals("More In Start-ups")) {
-                val elements = pageRowElement.select("div.Card-standardBreakerCard")
-
-                for (element in elements) {
-                    val title = element.selectFirst("a.Card-title")
-                    val datePost = element.selectFirst("span.Card-time")
-
-                    val dateFormat = SimpleDateFormat("EEE, MMM d yyyy")
-                    val dateString = datePost?.text()?.replace("st|nd|rd|th".toRegex(), "")
-                    val date = dateFormat.parse(dateString)
-
-                    responses.add(JsonObject().apply {
-                        put("title", title?.text())
-                        put("link", title?.attr("href"))
-                        put("timestamp", date.time)
-                    })
-                }
-            }
-        }
+        responses.addAll(scrapeCnbC())
+        responses.addAll(scrapeGeekWire())
+        responses.addAll(scrapeTechStartups())
 
         context.response().statusCode = 200
 
         context.response().putHeader("Content-Type", "application/json")
         context.response().end(responses.encode())
+    }
+
+    private fun scrapeCnbC(): JsonArray {
+        val responses = JsonArray()
+
+        val document = Jsoup.connect("https://www.cnbc.com/startups/").get()
+        val pageRowElements = document.select("div.PageBuilder-pageRow")
+
+        for (pageRowElement in pageRowElements) {
+            try {
+                val moduleHeaderTitle = pageRowElement.selectFirst("h2.ModuleHeader-title")
+
+                if (moduleHeaderTitle?.text().equals("More In Start-ups")) {
+                    val elements = pageRowElement.select("div.Card-standardBreakerCard")
+
+                    for (element in elements) {
+                        val title = element.selectFirst("a.Card-title")
+                        val datePost = element.selectFirst("span.Card-time")
+
+                        val dateFormat = SimpleDateFormat("EEE, MMM d yyyy")
+                        val dateString = datePost?.text()?.replace("st|nd|rd|th".toRegex(), "")
+                        val date = dateFormat.parse(dateString)
+
+                        responses.add(JsonObject().apply {
+                            put("title", title?.text())
+                            put("link", title?.attr("href"))
+                            put("timestamp", date.time)
+                        })
+                    }
+                }
+            } catch (e: Exception) {
+                logger.error(e.message)
+            }
+        }
+
+        return responses
+    }
+
+    private fun scrapeGeekWire(): JsonArray {
+        val responses = JsonArray()
+
+        val document = Jsoup.connect("https://www.geekwire.com/startups/").get()
+        val articleElements = document.select("article.type-post")
+
+        for (articleElement in articleElements) {
+            try {
+                val title = articleElement.selectFirst("h2.entry-title > a")
+                val datePost = articleElement.selectFirst("time.published")
+
+                val dateFormat = SimpleDateFormat("MMM d, yyyy")
+                val dateString = datePost?.text()
+                val date = dateFormat.parse(dateString)
+
+                responses.add(JsonObject().apply {
+                    put("title", title?.text())
+                    put("link", title?.attr("href"))
+                    put("timestamp", date.time)
+                })
+            } catch (e: Exception) {
+                logger.error(e.message)
+            }
+        }
+
+        return responses
+    }
+
+    private fun scrapeTechStartups(): JsonArray {
+        val responses = JsonArray()
+
+        val document = Jsoup.connect("https://techstartups.com/").get()
+        val postElements = document.select("div.post.type-post")
+
+        for (postElement in postElements) {
+            try {
+                val title = postElement.selectFirst("h5 > a")
+                val datePost = postElement.selectFirst("span.post_info_date")
+
+                val dateFormat = SimpleDateFormat("MMM d, yyyy")
+                val dateString = datePost?.text()?.replace("Posted On ", "")
+                val date = dateFormat.parse(dateString)
+
+                responses.add(JsonObject().apply {
+                    put("title", title?.text())
+                    put("link", title?.attr("href"))
+                    put("timestamp", date.time)
+                })
+            } catch (e: Exception) {
+                logger.error(e.message)
+            }
+        }
+
+        return responses
     }
 }
